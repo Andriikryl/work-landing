9b46ee97eb581d573749c99107f4260551206b43
Amdrii <andreyk557@gmail.com>
2023-03-30 15:33:01 +0300

    feat[mobile-menu]

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactComponent as LogoIcon } from '../../images/logo.svg'
 import { ReactComponent as MenuIcon } from '../../images/icon-menu.svg'
+import { ReactComponent as MenuIconClose } from '../../images/icon-close-menu.svg'
 import { Button } from '../button'
 import { NavItem } from '../nav-item'
 import { NavMenu } from '../nav-menu'
 import { COMPANY, FEATURES } from './constatnt'
+import { MobileMenu } from '../mobile-menu'
 
 export const Header = () => {
+  const [isMobileMenuOpen, setisMobileMenuOpen] = useState(false)
   return (
     <header className='flex items-center'>
         <LogoIcon className='cursor-pointer'/>
@@ -24,9 +27,10 @@ export const Header = () => {
       <Button>Loggin</Button>
       <Button hasBorder={true}>Register</Button>
         </div>
-        <div className='flex xl:hidden ml-auto cursor-pointer z-30'>
-    <MenuIcon/>
+        <div className='flex xl:hidden ml-auto cursor-pointer z-30' onClick={() => setisMobileMenuOpen(!isMobileMenuOpen)}>
+    {isMobileMenuOpen ? <MenuIconClose/> : <MenuIcon/>}
         </div>
+        <MobileMenu isOpen={isMobileMenuOpen} />
     </header>
   )
 }
