703099d36a3b2a07ac1fc4397429340042b2df0f
Amdrii <andreyk557@gmail.com>
2023-03-30 13:16:57 +0300

    feat[nav-items]

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,10 +4,13 @@ import { NavItem } from '../nav-item'
 
 export const Header = () => {
   return (
-    <header className='flex'>
+    <header className='flex items-center'>
         <LogoIcon/>
-        <nav>
-            <NavItem/>
+        <nav className='flex space-x-6 ml-8 items-center'>
+            <NavItem text='Feature'/>
+            <NavItem text='Company'/>
+            <NavItem text='Careers'/>
+            <NavItem text='about'/>
         </nav>
     </header>
   )
--- src/components/nav-item/index.jsx
+++ src/components/nav-item/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import {ReactComponent as ArrowDown} from '../../images/icon-arrow-down.svg'
 
-export const NavItem = () => {
+export const NavItem = ({text = ''}) => {
   return (
-    <div>asd</div>
+    <div className='relative'>
+        <div className='flex space-x-2 cursor-pointer items-center'>
+        <span className='text-medium-gray hover:text-almost-black'>{text}</span>
+            <ArrowDown/>
+        </div>
+    </div>
   )
 }
